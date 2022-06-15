906ddf55be482cef9cdee84372f5803fa5495d4c
Stephen Fudge <sfudge@gmail.com>
2022-06-15 17:25:50 -0400

    added a few lines to server.js

--- server.js
+++ server.js
@@ -6,11 +6,19 @@ const PORT = 3001 || process.env.PORT;
 // setting the static location for things being referenced
 app.use(express.static('public'));
 
-// url endpoints 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use('/api', );
-app.use('/', );
 
 
+// url endpoints 
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../public/notes.html'));
+  });
 
 // letting you know what port is being used
 app.listen(PORT, () =>
