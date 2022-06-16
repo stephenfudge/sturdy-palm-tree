1263e99b6409ab25d943befb4b2fe9b977f0ec0b
Stephen Fudge <sfudge@gmail.com>
2022-06-16 18:51:49 -0400

    made change to server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // declaring express and the port
 const express = require('express');
 const app = express();
-const PORT = 3001 || process.env.PORT;
+const PORT = process.env.PORT || 3001 ; 
 const path = require('path');
 const fs = require('fs');
 const uniqid = require('./routes/helper');
